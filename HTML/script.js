9f34f61843efdf5493c488b9178c21ce0714e45b
Ben <mrneverrr@gmail.com>
2021-12-07 15:21:20 +0100

    ajout algo bar animation

--- HTML/script.js
+++ HTML/script.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    var n = +document.querySelector("#compteur").getAttribute("data-value");
-    var cpt = 0;
-    var duree = 2;
-    var delta = Math.ceil((duree * 1000) / n);
-    var node = document.getElementById("compteur");
 
-    function countdown() {
-        node.innerHTML = ++cpt + "%";
-        if (cpt < n) {
-            setTimeout(countdown, delta);
+    setTimeout(barAnimation, 2000);
+});
+
+function barAnimation() {
+    const compteurs = document.querySelectorAll(".compteur");
+    for (let compteur of compteurs) {
+        let cpt = 0;
+        const n = +compteur.getAttribute("data-value");
+        const duree = 2;
+        const delta = Math.ceil((duree * 1000) / n);
+
+        function countdown() {
+            compteur.innerHTML = ++cpt + "%";
+            if (cpt < n) {
+                setTimeout(countdown, delta);
+            }
         }
-    }
+        countdown();
 
-    setTimeout(countdown, 2000);
-})
+    }
+}
