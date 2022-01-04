311c8a5ceee26255ee28e60428a570773a6597a3
Totodore <prevottheodore@gmail.com>
2022-01-04 14:16:13 +0100

    bug fix tries

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -43,7 +43,7 @@ export class MainController extends BaseController {
 	}
 
 	updateLoginStatus() {
-		console.log(this.apiService.logged);
+		this.log(this.apiService.logged ? "Logged" : "Not logged");
 		if (this.apiService.logged) {
 			this.select(".account").style.display = "block";
 			this.select(".connexion").style.display = "none";
