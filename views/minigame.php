76b640369312f910421f02650f14edd2e3a2ff7a
Ben <mrneverrr@gmail.com>
2022-01-15 20:40:19 +0100

    modif minigames

--- views/minigame.php
+++ views/minigame.php
@@ -1,9 +1,17 @@
 <div class='empsmiley'>
+        <div class="1smiley">
+            <?php include __DIR__. "/../public/images/1smiley.svg"
+            ?>
+        <div class="2smiley">
+            <?php include __DIR__. "/../public/images/2smiley.svg"
+            ?>
+        </div>
         <div class='1sound'>
-            <?php include __DIR__. "/../public/images/1sound.svg"
+           <?php  include __DIR__. "/../public/images/1sound.svg"
             ?>
 
         </div>
+       
        
 
 
