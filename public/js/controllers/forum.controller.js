b96252016fadc43c591340aba479e6f5d71f5d97
Saly <salymayu@gmail.com>
2022-01-15 15:18:47 +0100

    forum

--- public/js/controllers/forum.controller.js
+++ public/js/controllers/forum.controller.js
@@ -15,14 +15,5 @@ export class ForumController extends BaseController {
 	}
 
 	async onInit() {
-		const question = document.querySelectorAll('.forum-wrapper');
-		for (const questions of question) {
-            questions.querySelector('span.material-icons').addEventListener('click', () => {
-                const reponse = questions.querySelector('.reponse');
-                if (reponse.style.display == 'block')
-                    reponse.style.display = 'none';
-                else reponse.style.display = 'block';
-            });
-        }
 	}
 }
