19cae8b65b0b7e21ab7a8faf1f0ed2a5baebff7e
Totodore <prevottheodore@gmail.com>
2022-01-16 11:38:46 +0100

    admin page

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,3 +1,15 @@
+<h2>Administration du site</h2>
 <div class="admin-wrapper">
-	
+	<div>
+		<span class="material-icons">people</span>
+		<h3>Gestion des utilisateurs</h3>
+	</div>
+	<div>
+		<span class="material-icons">bug_report</span>
+		<h3>Gestion des tickets</h3>
+	</div>
+	<div>
+		<span class="material-icons">chat</span>
+		<h3>Gestion du forum</h3>
+	</div>
 </div>
