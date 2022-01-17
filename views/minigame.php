474b0bc56688ad4a6e5efdbab5e6d6aba07be12e
Ben <mrneverrr@gmail.com>
2022-01-17 10:10:20 +0100

    maj minigame ajur rating

--- views/minigame.php
+++ views/minigame.php
@@ -1,18 +1,26 @@
 <div class='empsmiley'>
-        <div class="1smiley">
-            <?php include __DIR__. "/../public/images/1smiley.svg"
-            ?>
-        <div class="2smiley">
-            <?php include __DIR__. "/../public/images/2smiley.svg"
-            ?>
-        </div>
-        <div class='1sound'>
-           <?php  include __DIR__. "/../public/images/1sound.svg"
-            ?>
-
-        </div>
-       
-       
-
+    <div class="1smiley">
+        <?php include __DIR__ . "/../public/images/1smiley.svg"
+        ?>
+    </div>
+    <div class="2smiley">
+        <?php include __DIR__ . "/../public/images/2smiley.svg"
+        ?>
+    </div>
+    <div class='3smiley'>
+        <?php include __DIR__ . "/../public/images/3smiley.svg"
+        ?>
 
     </div>
+    <div class=43smiley'>
+        <?php include __DIR__ . "/../public/images/4smiley.svg"
+        ?>
+
+    </div>
+    <div class='5smiley'>
+        <?php include __DIR__ . "/../public/images/5smiley.svg"
+        ?>
+
+    </div>
+
+</div>
