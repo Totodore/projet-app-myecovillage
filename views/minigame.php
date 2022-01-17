00808c574576d709e91621d442268eb7afe57d7c
Ben <mrneverrr@gmail.com>
2022-01-17 11:12:17 +0100

    minigame maj aout sound

--- views/minigame.php
+++ views/minigame.php
@@ -1,4 +1,7 @@
+
+
 <div class='empsmiley'>
+
     <div class="1smiley">
         <?php include __DIR__ . "/../public/images/1smiley.svg"
         ?>
@@ -12,7 +15,7 @@
         ?>
 
     </div>
-    <div class=43smiley'>
+    <div class='4smiley'>
         <?php include __DIR__ . "/../public/images/4smiley.svg"
         ?>
 
@@ -23,4 +26,72 @@
 
     </div>
 
+</div>
+<div class='empsoundnight'>
+<div class="moon">
+        <?php include __DIR__ . "/../public/images/moon.svg"
+        ?>
+</div>
+    <div class='soundicon'>
+    <div class='sound1'>
+    <?php include __DIR__ . "/../public/images/1sound.svg"
+        ?>
+
+    </div>
+    <div class='sound2'>
+    <?php include __DIR__ . "/../public/images/2sound.svg"
+        ?>
+
+    </div>
+    <div class='sound3'>
+    <?php include __DIR__ . "/../public/images/3sound.svg"
+        ?>
+
+    </div>
+    <div class='sound4'>
+    <?php include __DIR__ . "/../public/images/4sound.svg"
+        ?>
+
+    </div>
+    <div class='sound5'>
+    <?php include __DIR__ . "/../public/images/5sound.svg"
+        ?>
+
+    </div>
+</div>
+
+</div>
+<div class='empsoundday'>
+<div class="moon">
+        <?php include __DIR__ . "/../public/images/sun.svg"
+        ?>
+</div>
+<div class='soundicon'>
+    <div class='sound1'>
+    <?php include __DIR__ . "/../public/images/1sound.svg"
+        ?>
+
+    </div>
+    <div class='sound2'>
+    <?php include __DIR__ . "/../public/images/2sound.svg"
+        ?>
+
+    </div>
+    <div class='sound3'>
+    <?php include __DIR__ . "/../public/images/3sound.svg"
+        ?>
+
+    </div>
+    <div class='sound4'>
+    <?php include __DIR__ . "/../public/images/4sound.svg"
+        ?>
+
+    </div>
+    <div class='sound5'>
+    <?php include __DIR__ . "/../public/images/5sound.svg"
+        ?>
+
+    </div>
+
+</div>
 </div>
