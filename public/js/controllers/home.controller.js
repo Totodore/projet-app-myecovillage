cc7a159d5f89320b5c5dbf174fe93b511c55c610
Totodore <prevottheodore@gmail.com>
2022-01-17 11:40:35 +0100

    home

--- public/js/controllers/home.controller.js
+++ public/js/controllers/home.controller.js
@@ -4,36 +4,37 @@ import { ApiService } from '../services/api.service.js';
 
 export class HomeController extends BaseController {
 
-    id = "home";
+	id = "home";
 
-    /**
-     * @param {ApiService} apiService
-     */
-    constructor(params, apiService) {
-        super("home", params);
-        this.apiService = apiService;
-    }
+	/**
+	 * @param {ApiService} apiService
+	 */
+	constructor(params, apiService) {
+		super("home", params);
+		this.apiService = apiService;
+	}
 
-    async onInit() {
-        setTimeout(() => this.barAnimation(), 2000);
+	async onInit() {
+		setTimeout(() => this.barAnimation(), 2000);
+		if (this.select(".no-data-link"))
+			this.navigate("minigame", ".no-data-link");
+	}
+	barAnimation() {
+		const compteurs = document.querySelectorAll(".compteur");
+		for (let compteur of compteurs) {
+			let cpt = 0;
+			const n = +compteur.getAttribute("data-value");
+			const duree = 2;
+			const delta = Math.ceil((duree * 1000) / n);
 
-    }
-    barAnimation() {
-        const compteurs = document.querySelectorAll(".compteur");
-        for (let compteur of compteurs) {
-            let cpt = 0;
-            const n = +compteur.getAttribute("data-value");
-            const duree = 2;
-            const delta = Math.ceil((duree * 1000) / n);
+			function countdown() {
+				compteur.innerHTML = ++cpt + "%";
+				if (cpt < n) {
+					setTimeout(countdown, delta);
+				}
+			}
+			countdown();
 
-            function countdown() {
-                compteur.innerHTML = ++cpt + "%";
-                if (cpt < n) {
-                    setTimeout(countdown, delta);
-                }
-            }
-            countdown();
-
-        }
-    }
+		}
+	}
 }
--- views/home.php
+++ views/home.php
@@ -1,117 +1,107 @@
-
+<h2>Bonjour <?php echo $user->firstname; ?>, voici vos informations quotidiennes.</h2>
 <div Class="Donnee">
-        <div Class="rond1">
-            <p id="Txtrond1">Donnée de </br>Température</p>
-            <div class="flex-wrapper">
-                <div class="single-chart">
-                    <svg viewBox="0 0 36 36" class="circular-chart Temp">
-                    <path class="circle-bg"
-                      d="M18 2.0845
+	<div Class="rond1">
+		<p id="Txtrond1">Température</p>
+		<div class="flex-wrapper">
+			<div class="single-chart">
+				<svg viewBox="0 0 36 36" class="circular-chart Temp">
+					<path class="circle-bg" d="M18 2.0845
                         a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <path class="circle"
-                      stroke-dasharray="30, 100"
-                      d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 -31.831" />
+					<path class="circle" stroke-dasharray="30, 100" d="M18 2.0845
                         a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <text x="18" y="20.35" class="percentage">30°C</text>
-                  </svg>
-                </div>
-            </div>
-        </div>
-        <div Class="rond2">
-            <p id="Txtrond2">Donnée de </br>CO2</p>
-            <div class="flex-wrapper">
-                <div class="single-chart">
-                    <svg viewBox="0 0 36 36" class="circular-chart CO2">
-                    <path class="circle-bg"
-                      d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 -31.831" />
+					<text x="18" y="20.35" class="percentage">30°C</text>
+				</svg>
+			</div>
+		</div>
+	</div>
+	<div Class="rond2">
+		<p id="Txtrond2">Co2</p>
+		<div class="flex-wrapper">
+			<div class="single-chart">
+				<svg viewBox="0 0 36 36" class="circular-chart CO2">
+					<path class="circle-bg" d="M18 2.0845
                         a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <path class="circle"
-                      stroke-dasharray="60, 100"
-                      d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 -31.831" />
+					<path class="circle" stroke-dasharray="60, 100" d="M18 2.0845
                         a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <text x="18" y="20.35" class="percentage">50 µg</text> 
-                    
-                  </svg>
-                </div>
-            </div>
-        </div>
-        <div Class="rond3">
-            <p id="Txtrond3">Donnée de </br>Bruit</p>
-            <div class="flex-wrapper">
-                <div class="single-chart">
-                    <svg viewBox="0 0 36 36" class="circular-chart Decibel">
-                    <path class="circle-bg"
-                      d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <path class="circle"
-                      stroke-dasharray="80, 100"
-                      d="M18 2.0845
-                        a 15.9155 15.9155 0 0 1 0 31.831
-                        a 15.9155 15.9155 0 0 1 0 -31.831"
-                    />
-                    <text x="18" y="20.35" class="percentage">100Db</text> 
-                    
-                  </svg>
-                </div>
-            </div>
-        </div>
+                        a 15.9155 15.9155 0 0 1 0 -31.831" />
+					<text x="18" y="20.35" class="percentage">50 µg</text>
 
+				</svg>
+			</div>
+		</div>
+	</div>
+	<div Class="rond3">
+		<p id="Txtrond3">Bruit</p>
+		<div class="flex-wrapper">
+			<div class="single-chart">
+				<svg viewBox="0 0 36 36" class="circular-chart Decibel">
+					<path class="circle-bg" d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831" />
+					<path class="circle" stroke-dasharray="80, 100" d="M18 2.0845
+                        a 15.9155 15.9155 0 0 1 0 31.831
+                        a 15.9155 15.9155 0 0 1 0 -31.831" />
+					<text x="18" y="20.35" class="percentage">100Db</text>
 
-    </div>
-</body>
-<div class="Emptableau">
-    <div class="Tableau">
-        <div id="graph1">
-            <div id="ptab1">
-                <p class="compteur" data-value="80">0</p>
-            </div>
-        </div>
-        <div id="barre_tableausmall"></div>
-        <div id="Txtgraph1_2">Lundi</div>
-
-        <div id="graph2">
-            <div id="ptab1">
-                <p class="compteur" data-value="70">0</p>
-            </div>
-        </div>
-        <div id="barre_tableausmall"></div>
-        <div id="Txtgraph2_2">Mardi</div>
-        <div id="graph3">
-            <!---<pan id="Txtgraph3">Mercredi</pan> -->
-        </div>
-        <div id="graph4">
-            <!---<pan id="Txtgraph4">Jeudi</pan> -->
-        </div>
-        <div id="graph5">
-            <!---<pan id="Txtgraph5">Vendredi</pan> -->
-        </div>
-        <div id="graph6">
-            <!---<pan id="Txtgraph6">Samedi</pan> -->
-        </div>
-        <div id="graph7">
-            <!--- <pan id="Txtgraph7">Dimanche</pan> -->
-        </div>
-    </div>
-    <div id="barre_tableau"></div>
-
-
-    <div class="Txttableau">
-        <div id="Txtgraph1">Lundi</div>
-        <div id="Txtgraph2">Mardi</div>
-        <div id="Txtgraph2">Mercredi</div>
-        <div id="Txtgraph2">Jeudi</div>
-        <div id="Txtgraph2">Vendredi</div>
-        <div id="Txtgraph2">Samedi</div>
-        <div id="Txtgraph2">Dimanche</div>
-    </div>
+				</svg>
+			</div>
+		</div>
+	</div>
 </div>
+
+<?php if ($hasDayStat) { ?>
+	<div class="Emptableau">
+		<div class="Tableau">
+			<div id="graph1">
+				<div id="ptab1">
+					<p class="compteur" data-value="80">0</p>
+				</div>
+			</div>
+			<div id="barre_tableausmall"></div>
+			<div id="Txtgraph1_2">Lundi</div>
+
+			<div id="graph2">
+				<div id="ptab1">
+					<p class="compteur" data-value="70">0</p>
+				</div>
+			</div>
+			<div id="barre_tableausmall"></div>
+			<div id="Txtgraph2_2">Mardi</div>
+			<div id="graph3">
+				<span id="Txtgraph3">Mercredi</span>
+			</div>
+			<div id="graph4">
+				<span id="Txtgraph4">Jeudi</span>
+			</div>
+			<div id="graph5">
+				<span id="Txtgraph5">Vendredi</span>
+			</div>
+			<div id="graph6">
+				<span id="Txtgraph6">Samedi</span>
+			</div>
+			<div id="graph7">
+				<span id="Txtgraph7">Dimanche</span>
+			</div>
+		</div>
+		<div id="barre_tableau"></div>
+
+
+		<div class="Txttableau">
+			<div id="Txtgraph1">Lundi</div>
+			<div id="Txtgraph2">Mardi</div>
+			<div id="Txtgraph2">Mercredi</div>
+			<div id="Txtgraph2">Jeudi</div>
+			<div id="Txtgraph2">Vendredi</div>
+			<div id="Txtgraph2">Samedi</div>
+			<div id="Txtgraph2">Dimanche</div>
+		</div>
+	</div>
+<?php } else { ?>
+	<h2 class="no-data">
+		Impossible d'établir votre profil au sein de votre écoquartier, nous vous invitons à participer à
+		<button class="no-data-link">un minijeu quotidien.</button>
+	</h2>
+<?php } ?>
