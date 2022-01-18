0f4070f9353de689de7f8118dde6f4a458a2759e
Jane <jasiewe21@gmail.com>
2022-01-18 15:11:13 +0100

    update faq

--- views/faq.php
+++ views/faq.php
@@ -30,18 +30,7 @@
         </div>
         <div class="toggle-panel">
             <h4>Où trouver une borne "MyEcoVillage" dans mon ecoquartier ?</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elementum sed sem a dictum. Nam libero turpis, tempus sit amet ligula in, ultrices rutrum nibh. Aliquam eget magna ut turpis eleifend congue. Mauris molestie nisl luctus nunc auctor mollis. Nulla facilisi. Cras ac condimentum mi, rhoncus rhoncus libero. Vivamus placerat efficitur convallis. Praesent sed varius lectus. Donec congue in metus a dignissim. </p>
-        </div>
-    </div>
-
-    <div class="questions">
-        <div class="visible-panel">
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <span class="material-icons">add</span>
-        </div>
-        <div class="toggle-panel">
-            <h4>Lorem ipsum dolor sit amet</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elementum sed sem a dictum. Nam libero turpis, tempus sit amet ligula in, ultrices rutrum nibh. </p>
+            <p>Vous pouvez vous rendre sur la page d'<button class="test">accueil</button> du site pour géolocaliser une borne à proximiié. </p>
         </div>
     </div>
 </div>
