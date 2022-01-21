d118ba0fcc3f6f334bb37f2cc01adfe74f99214c
Totodore <prevottheodore@gmail.com>
2022-01-21 20:16:30 +0100

    patch bgsite and reformat

--- views/index.php
+++ views/index.php
@@ -1,19 +1,20 @@
 <html>
-	<head>
-		<?php include "components/head.php"; ?>
-	</head>
-	<body>
-		<?php include "components/header.php"; ?>
-		<div id="body-wrapper"></div>
-		<div class='bgsite'>
-    <?php include __DIR__ . "/../public/images/bgsite.svg"
-        ?>
-</div>
-<div class='bgsite2'>
-    <?php include __DIR__ . "/../public/images/bgsite2.svg"
-        ?>
-</div>
 
-		<?php include "components/footer.php"; ?>
-	</body>
+<head>
+	<?php include "components/head.php"; ?>
+</head>
+
+<body>
+	<?php include "components/header.php"; ?>
+	<div id="body-wrapper"></div>
+	<div class='bgsite'>
+		<?php include __DIR__ . "/../public/images/bgsite.svg" ?>
+	</div>
+	<div class='bgsite2'>
+		<?php include __DIR__ . "/../public/images/bgsite2.svg" ?>
+	</div>
+
+	<?php include "components/footer.php"; ?>
+</body>
+
 </html>
